5c56c149ad83ac8c5260b3f095c794e61224a2bc
Aman Kubanychbek <amankubanychbek@gmail.com>
2020-05-13 16:14:05 +0600

    Updates

---
+++ precache-manifest.640da3e8604637344b404c42ec0e49c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9241aca6a9d1030738dada83899bc71",
+    "url": "/test-jubilant-octo-tribble/index.html"
+  },
+  {
+    "revision": "bb3d4e170094997cf783",
+    "url": "/test-jubilant-octo-tribble/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "be3676ca2f527aaf22d5",
+    "url": "/test-jubilant-octo-tribble/static/js/2.03f71503.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-jubilant-octo-tribble/static/js/2.03f71503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3d4e170094997cf783",
+    "url": "/test-jubilant-octo-tribble/static/js/main.cdd33e9e.chunk.js"
+  },
+  {
+    "revision": "107951f691777b9809b8",
+    "url": "/test-jubilant-octo-tribble/static/js/runtime-main.69559d9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-jubilant-octo-tribble/static/media/logo.5d5d9eef.svg"
+  }
+]);
